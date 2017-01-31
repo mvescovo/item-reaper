0190bfd1b7fbffd7031a34658023974f48b8eeaa
Michael Vescovo <m.vescovo@gmail.com>
2017-02-01 04:40:23 +1100

    feat: Add Dagger2 DataSourceModule

---
+++ app/src/mock/java/com/michaelvescovo/android/itemreaper/data/DataSourceModule.java
@@ -0,0 +1,17 @@
+package com.michaelvescovo.android.itemreaper.data;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * @author Michael Vescovo
+ */
+
+@Module
+class DataSourceModule {
+
+    @Provides
+    static DataSource provideDataSource() {
+        return new FakeDataSource();
+    }
+}
