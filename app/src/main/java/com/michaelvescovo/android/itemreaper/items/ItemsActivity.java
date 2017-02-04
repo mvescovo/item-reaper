eb755da83fcb869d96cc78915fb2512a948523a1
Michael Vescovo <m.vescovo@gmail.com>
2017-02-05 08:14:39 +1100

    feat: Add ItemsActivity

---
+++ app/src/main/java/com/michaelvescovo/android/itemreaper/items/ItemsActivity.java
@@ -0,0 +1,31 @@
+package com.michaelvescovo.android.itemreaper.items;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.michaelvescovo.android.itemreaper.R;
+
+public class ItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
