4641158b84aa4d2882b59fffc091dbda59eb8295
Michael Vescovo <m.vescovo@gmail.com>
2017-02-05 07:17:20 +1100

    feat: Add AuthPresenter

---
+++ app/src/main/java/com/michaelvescovo/android/itemreaper/auth/AuthPresenter.java
@@ -0,0 +1,51 @@
+package com.michaelvescovo.android.itemreaper.auth;
+
+import javax.inject.Inject;
+
+/**
+ * @author Michael Vescovo
+ */
+
+class AuthPresenter implements AuthContract.Presenter {
+
+    private AuthContract.View mView;
+
+    @Inject
+    AuthPresenter(AuthContract.View view) {
+        mView = view;
+    }
+
+    @Inject
+    void setupListeners() {
+        mView.setPresenter(this);
+    }
+
+    @Override
+    public void googleSignIn() {
+        mView.showSignInButton(false);
+        mView.setProgressIndicator(true);
+        mView.showGoogleSignInUi();
+    }
+
+    @Override
+    public void handleGoogleSignInResult(boolean signedIn) {
+        if (signedIn) {
+            mView.showFireBaseAuthUi();
+        } else {
+            mView.setProgressIndicator(false);
+            mView.showSignInButton(true);
+            mView.showFailMessage();
+        }
+    }
+
+    @Override
+    public void handleFirebaseSignInResult(boolean signedIn) {
+        mView.setProgressIndicator(false);
+        if (signedIn) {
+            mView.showItemsUi();
+        } else {
+            mView.showFailMessage();
+            mView.showSignInButton(true);
+        }
+    }
+}
