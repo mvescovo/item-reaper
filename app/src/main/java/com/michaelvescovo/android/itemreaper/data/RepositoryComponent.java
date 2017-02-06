1eb8c4af443d9aa89dbb46905dc5d978da1008d6
Michael Vescovo <m.vescovo@gmail.com>
2017-02-06 18:08:35 +1100

    style: Add Singleton annotation
    Even though the code itself makes the singleton it can be helpful to
    know by looking at the annotation.

--- app/src/main/java/com/michaelvescovo/android/itemreaper/data/RepositoryComponent.java
+++ app/src/main/java/com/michaelvescovo/android/itemreaper/data/RepositoryComponent.java
@@ -1,11 +1,14 @@
 package com.michaelvescovo.android.itemreaper.data;
 
+import javax.inject.Singleton;
+
 import dagger.Component;
 
 /**
  * @author Michael Vescovo
  */
 
+@Singleton
 public @Component(modules = DataSourceModule.class)
 interface RepositoryComponent {
 
