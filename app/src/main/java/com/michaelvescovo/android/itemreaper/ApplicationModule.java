38b7bf956e0566cefa128a7929aab358c6a61680
Michael Vescovo <m.vescovo@gmail.com>
2017-02-08 18:55:53 +1100

    feat: Enable night mode
    Set to follow system.

--- app/src/main/java/com/michaelvescovo/android/itemreaper/ApplicationModule.java
+++ app/src/main/java/com/michaelvescovo/android/itemreaper/ApplicationModule.java
@@ -1,6 +1,7 @@
 package com.michaelvescovo.android.itemreaper;
 
 import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatDelegate;
 
 import dagger.Module;
 import dagger.Provides;
@@ -13,6 +14,10 @@ import dagger.Provides;
 class ApplicationModule {
 
     private SharedPreferences mSharedPreferences;
+
+    static {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+    }
 
     ApplicationModule(SharedPreferences sharedPreferences) {
         mSharedPreferences = sharedPreferences;
