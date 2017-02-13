e2c9f94fb5e09a63f72aeb4fd0d3ecf67593c0d1
Michael Vescovo <m.vescovo@gmail.com>
2017-02-13 12:13:14 +1100

    test: Add tests to see all  fields

--- app/src/androidTest/java/com/michaelvescovo/android/itemreaper/edit_item/EditItemScreenTest.java
+++ app/src/androidTest/java/com/michaelvescovo/android/itemreaper/edit_item/EditItemScreenTest.java
@@ -11,9 +11,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.Assert.fail;
 
 /**
  * @author Michael Vescovo
@@ -25,7 +27,7 @@ public class EditItemScreenTest {
 
     @Rule
     public IntentsTestRule<EditItemActivity> mActivityRule =
-            new IntentsTestRule<EditItemActivity>(EditItemActivity.class);
+            new IntentsTestRule<>(EditItemActivity.class);
 
     @Test
     public void saveMenuOptionVisible() {
@@ -33,7 +35,97 @@ public class EditItemScreenTest {
     }
 
     @Test
+    public void purchaseDateEditTextVisible() {
+        onView(withId(R.id.edit_purchase_date)).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void pricePaidEditTextVisible() {
+        onView(withId(R.id.edit_price_paid)).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void discountEditTextVisible() {
+        onView(withId(R.id.edit_discount)).perform(scrollTo()).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void expiryEditTextVisible() {
+        onView(withId(R.id.edit_expiry)).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void categoryEditTextVisible() {
+        onView(withId(R.id.edit_category)).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void subCategoryEditTextVisible() {
+        onView(withId(R.id.edit_sub_category)).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void typeEditTextVisible() {
+        onView(withId(R.id.edit_type)).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void subTypeEditTextVisible() {
+        onView(withId(R.id.edit_sub_type)).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void subType2EditTextVisible() {
+        onView(withId(R.id.edit_sub_type2)).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void subType3EditTextVisible() {
+        onView(withId(R.id.edit_sub_type3)).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void primaryColourEditTextVisible() {
+        onView(withId(R.id.edit_primary_colour)).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void primaryColourShadeEditTextVisible() {
+        onView(withId(R.id.edit_primary_colour_shade)).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void secondaryColourEditTextVisible() {
+        onView(withId(R.id.edit_secondary_colour)).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void sizeEditTextVisible() {
+        onView(withId(R.id.edit_size)).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void brandEditTextVisible() {
+        onView(withId(R.id.edit_brand)).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void shopEditTextVisible() {
+        onView(withId(R.id.edit_shop)).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void descriptionEditTextVisible() {
+        onView(withId(R.id.edit_description)).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void noteEditTextVisible() {
+        onView(withId(R.id.edit_note)).perform(scrollTo()).check(matches(isDisplayed()));
+    }
+
+    @Test
     public void clickSave_ShowsItemInList() {
-        // Can't test this until the items feature is working
+        fail("Can't test this until the items feature is working");
     }
 }
