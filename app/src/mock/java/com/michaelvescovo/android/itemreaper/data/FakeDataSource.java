68b47bf465d4d25d0e587be8cb5107771e4cdfa1
Michael Vescovo <m.vescovo@gmail.com>
2017-02-14 20:10:31 +1100

    refactor: Allow access to the fake dataSource
    All test classes to be able to use the data in the FakeDataSource
    class.

--- app/src/mock/java/com/michaelvescovo/android/itemreaper/data/FakeDataSource.java
+++ app/src/mock/java/com/michaelvescovo/android/itemreaper/data/FakeDataSource.java
@@ -1,6 +1,7 @@
 package com.michaelvescovo.android.itemreaper.data;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -13,17 +14,19 @@ import java.util.UUID;
  * @author Michael Vescovo
  */
 
+@VisibleForTesting
+@SuppressWarnings("WeakerAccess")
 public class FakeDataSource implements DataSource {
 
     public final static String USER_ID = "testUser";
-    private final static List<String> ITEM_IDS = Lists.newArrayList();
-    private final static Map<String, Item> ITEMS = Maps.newHashMap();
-    private final static String ITEM_ID_1 = "1";
-    public final static Item ITEM_1 = new Item("-1", null, 20, 0, "1/1/1", "Clothing", null,
+    public final static List<String> ITEM_IDS = Lists.newArrayList();
+    public final static Map<String, Item> ITEMS = Maps.newHashMap();
+    public final static String ITEM_ID_1 = "1";
+    public final static Item ITEM_1 = new Item("-1", -1, 20, -1, 1, "Clothing", null,
             "T-shirt", null, null, null, "Black", null, null, null, null, null, null, null, null,
             false);
-    private final static String ITEM_ID_2 = "2";
-    public final static Item ITEM_2 = new Item("2", null, 30, 0, "2/2/2", "Bathroom", null,
+    public final static String ITEM_ID_2 = "2";
+    public final static Item ITEM_2 = new Item("2", -1, 30, -1, 2, "Bathroom", null,
             "Towel", null, null, null, "White", null, null, null, null, null, null, null, null,
             false);
 
