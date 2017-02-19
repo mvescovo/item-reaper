cf99a1781a104047ab4698b72552ea9f569b8091
Michael Vescovo <m.vescovo@gmail.com>
2017-02-20 03:09:08 +1100

    refactor: Use different ImageFile for mock

---
+++ app/src/mock/java/com/michaelvescovo/android/itemreaper/edit_item/EditItemModule.java
@@ -0,0 +1,31 @@
+package com.michaelvescovo.android.itemreaper.edit_item;
+
+import com.michaelvescovo.android.itemreaper.util.FakeImageFileImpl;
+import com.michaelvescovo.android.itemreaper.util.ImageFile;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * @author Michael Vescovo
+ */
+
+@Module
+public class EditItemModule {
+
+    private EditItemContract.View mView;
+
+    public EditItemModule(EditItemContract.View view) {
+        mView = view;
+    }
+
+    @Provides
+    EditItemContract.View provideEditItemView() {
+        return mView;
+    }
+
+    @Provides
+    ImageFile provideImageFile() {
+        return new FakeImageFileImpl();
+    }
+}
--- app/src/prod/java/com.michaelvescovo.android.itemreaper/edit_item/EditItemModule.java
+++ app/src/prod/java/com.michaelvescovo.android.itemreaper/edit_item/EditItemModule.java
@@ -1,5 +1,9 @@
 package com.michaelvescovo.android.itemreaper.edit_item;
 
+import com.michaelvescovo.android.itemreaper.util.ImageFileImpl;
+import com.michaelvescovo.android.itemreaper.edit_item.EditItemContract;
+import com.michaelvescovo.android.itemreaper.util.ImageFile;
+
 import dagger.Module;
 import dagger.Provides;
 
@@ -19,5 +23,10 @@ public class EditItemModule {
     @Provides
     EditItemContract.View provideEditItemView() {
         return mView;
+    }
+
+    @Provides
+    ImageFile provideImageFile() {
+        return new ImageFileImpl();
     }
 }
