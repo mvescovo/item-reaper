e1161376da886a329219ec0e70e3ca1d09eed2f5
Michael Vescovo <m.vescovo@gmail.com>
2017-03-07 14:20:07 +1100

    test: Add rotation to About screen tests

--- app/src/androidTest/java/com/michaelvescovo/android/itemreaper/about/AboutScreenTest.java
+++ app/src/androidTest/java/com/michaelvescovo/android/itemreaper/about/AboutScreenTest.java
@@ -1,5 +1,10 @@
 package com.michaelvescovo.android.itemreaper.about;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -30,30 +35,53 @@ public class AboutScreenTest {
     @Test
     public void titleVisible() {
         onView(withText(R.string.title_activity_about)).check(matches(isDisplayed()));
+        rotateScreen();
+        onView(withText(R.string.title_activity_about)).check(matches(isDisplayed()));
     }
 
     @Test
     public void itemReaperTitleVisible() {
+        onView(withText(R.string.title_item_reaper)).check(matches(isDisplayed()));
+        rotateScreen();
         onView(withText(R.string.title_item_reaper)).check(matches(isDisplayed()));
     }
 
     @Test
     public void openSourceStatementVisible() {
         onView(withText(R.string.open_source_statement)).check(matches(isDisplayed()));
+        rotateScreen();
+        onView(withText(R.string.open_source_statement)).check(matches(isDisplayed()));
     }
 
     @Test
     public void forkStatementVisible() {
+        onView(withText(R.string.fork_statement)).check(matches(isDisplayed()));
+        rotateScreen();
         onView(withText(R.string.fork_statement)).check(matches(isDisplayed()));
     }
 
     @Test
     public void attributionsTitleVisible() {
         onView(withText(R.string.title_attributions)).check(matches(isDisplayed()));
+        rotateScreen();
+        onView(withText(R.string.title_attributions)).check(matches(isDisplayed()));
     }
 
     @Test
     public void reaperIconAttributionVisible() {
         onView(withText(R.string.reaper_icon_attribution)).check(matches(isDisplayed()));
+        rotateScreen();
+        onView(withText(R.string.reaper_icon_attribution)).check(matches(isDisplayed()));
+    }
+
+    private void rotateScreen() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        int orientation = context.getResources().getConfiguration().orientation;
+        Activity activity = mActivityRule.getActivity();
+        activity.setRequestedOrientation(
+                orientation == Configuration.ORIENTATION_PORTRAIT
+                        ? ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                        : ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        );
     }
 }
