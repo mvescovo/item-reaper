cca9ad5bf077677ee479381cfe5ea332b36dbb6d
Michael Vescovo <m.vescovo@gmail.com>
2017-03-09 08:25:35 +1100

    test: Sometimes tests fail after rotation
    So far I'm not sure of the exact cause or if I should be running the
    tests differently, but putting a long enough delay after rotating
    does work. Normally 500ms is enough but even then occasionally
    some fail. Even at 1500ms! So far nothing has ever failed with
    2000ms which seems very high.

--- app/src/main/java/com/michaelvescovo/android/itemreaper/util/RotationHelper.java
+++ app/src/main/java/com/michaelvescovo/android/itemreaper/util/RotationHelper.java
@@ -5,12 +5,15 @@ import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 
+import static java.lang.Thread.sleep;
+
 /**
  * @author Michael Vescovo
  */
 
 public class RotationHelper {
 
+    private static final int ROTATION_TIME_OUT = 500;
     private Context mContext;
     private Activity mActivity;
 
@@ -26,15 +29,28 @@ public class RotationHelper {
                         ? ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
                         : ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         );
+        sleepAfterRotation(ROTATION_TIME_OUT);
     }
 
     public void setPortrait() {
         mActivity.setRequestedOrientation(
                 ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        sleepAfterRotation(ROTATION_TIME_OUT);
     }
 
     public void setLandscape() {
         mActivity.setRequestedOrientation(
                 ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        sleepAfterRotation(ROTATION_TIME_OUT);
+    }
+
+    // Sometimes after rotating the device in Espresso it can't close the keyboard. Putting a delay
+    // seems to be an effective workaround. However, a better workaround or fix would be great!
+    private static void sleepAfterRotation(int ms) {
+        try {
+            sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
