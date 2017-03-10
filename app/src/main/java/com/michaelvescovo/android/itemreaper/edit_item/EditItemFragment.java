dc142b7d13fd0d92e1ef5474a2599f0662a8fbef
Michael Vescovo <m.vescovo@gmail.com>
2017-03-10 23:42:20 +1100

    fix: Comment out test line
    Forgot.

--- app/src/main/java/com/michaelvescovo/android/itemreaper/edit_item/EditItemFragment.java
+++ app/src/main/java/com/michaelvescovo/android/itemreaper/edit_item/EditItemFragment.java
@@ -168,7 +168,7 @@ public class EditItemFragment extends AppCompatDialogFragment implements EditIte
         configureViews();
 
         /* TEMP */
-        mItemId = "1";
+//        mItemId = "1";
 
         return root;
     }
