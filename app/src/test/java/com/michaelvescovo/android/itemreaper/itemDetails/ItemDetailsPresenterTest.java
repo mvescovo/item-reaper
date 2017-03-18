2586daf96b8760e726797fd0ec1c7932de3a7ab5
Michael Vescovo <m.vescovo@gmail.com>
2017-03-18 23:33:25 +1100

    fix: ItemDetailsPresenterTest not setup

--- app/src/test/java/com/michaelvescovo/android/itemreaper/itemDetails/ItemDetailsPresenterTest.java
+++ app/src/test/java/com/michaelvescovo/android/itemreaper/itemDetails/ItemDetailsPresenterTest.java
@@ -1,7 +1,21 @@
 package com.michaelvescovo.android.itemreaper.itemDetails;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.michaelvescovo.android.itemreaper.SharedPreferencesHelper;
+import com.michaelvescovo.android.itemreaper.data.Item;
+import com.michaelvescovo.android.itemreaper.data.Repository;
+
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+
+import static com.michaelvescovo.android.itemreaper.data.FakeDataSource.ITEM_1;
+import static com.michaelvescovo.android.itemreaper.data.FakeDataSource.ITEM_2;
 
 /**
  * @author Michael Vescovo
@@ -10,4 +24,43 @@ import org.junit.runners.Parameterized;
 @RunWith(Parameterized.class)
 public class ItemDetailsPresenterTest {
 
+    private ItemDetailsPresenter mItemDetailsPresenter;
+
+    @Mock
+    private ItemDetailsContract.View mView;
+
+    @Mock
+    private Repository mRepository;
+
+    @Mock
+    private SharedPreferencesHelper mSharedPreferencesHelper;
+
+    @Mock
+    private FirebaseAuth mFirebaseAuth;
+
+    @Parameterized.Parameters
+    public static Iterable<?> data() {
+        return Arrays.asList(
+                ITEM_1,
+                ITEM_2
+        );
+    }
+
+    private Item mItem;
+
+    public ItemDetailsPresenterTest(Item item) {
+        mItem = item;
+    }
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mItemDetailsPresenter = new ItemDetailsPresenter(mView, mRepository,
+                mSharedPreferencesHelper, mFirebaseAuth);
+    }
+
+    @Test
+    public void test() {
+
+    }
 }
