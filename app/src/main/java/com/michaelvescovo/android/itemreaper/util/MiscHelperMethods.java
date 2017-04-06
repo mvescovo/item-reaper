e01c1a71c3a4edbd1d6900de9643da76be54325c
Michael Vescovo <m.vescovo@gmail.com>
2017-04-06 19:58:19 +1000

    feat: Add to and from byte[] helper methods
    For some reason the system won't let me pass a serializable or
    parcelable in an intent to a widget list service.

--- app/src/main/java/com/michaelvescovo/android/itemreaper/util/MiscHelperMethods.java
+++ app/src/main/java/com/michaelvescovo/android/itemreaper/util/MiscHelperMethods.java
@@ -1,5 +1,10 @@
 package com.michaelvescovo.android.itemreaper.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
@@ -22,5 +27,65 @@ public class MiscHelperMethods {
 
     public static SimpleDateFormat getDateFormat(String format) {
         return new SimpleDateFormat(format, Locale.getDefault());
+    }
+
+    public static byte[] toByteArray(Object obj) {
+        byte[] bytes = null;
+        ByteArrayOutputStream bos = null;
+        ObjectOutputStream oos = null;
+        try {
+            bos = new ByteArrayOutputStream();
+            oos = new ObjectOutputStream(bos);
+            oos.writeObject(obj);
+            oos.flush();
+            bytes = bos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (oos != null) {
+                try {
+                    oos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return bytes;
+    }
+
+    public static Object toObject(byte[] bytes) {
+        Object obj = null;
+        ByteArrayInputStream bis = null;
+        ObjectInputStream ois = null;
+        try {
+            bis = new ByteArrayInputStream(bytes);
+            ois = new ObjectInputStream(bis);
+            obj = ois.readObject();
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bis != null) {
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ois != null) {
+                try {
+                    ois.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return obj;
     }
 }
