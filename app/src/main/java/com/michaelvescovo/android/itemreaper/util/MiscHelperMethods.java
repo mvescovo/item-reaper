be7874f3c0c078fc0d9337ed60abb33c9f77d2a6
Michael Vescovo <m.vescovo@gmail.com>
2017-04-13 00:53:55 +1000

    style: Remove redundant helper methods

--- app/src/main/java/com/michaelvescovo/android/itemreaper/util/MiscHelperMethods.java
+++ app/src/main/java/com/michaelvescovo/android/itemreaper/util/MiscHelperMethods.java
@@ -1,10 +1,5 @@
 package com.michaelvescovo.android.itemreaper.util;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
@@ -29,45 +24,5 @@ public class MiscHelperMethods {
 
     public static SimpleDateFormat getDateFormat(String format) {
         return new SimpleDateFormat(format, Locale.ENGLISH);
-    }
-
-    public static byte[] toByteArray(Object obj) throws IOException {
-        byte[] bytes = null;
-        ByteArrayOutputStream bos = null;
-        ObjectOutputStream oos = null;
-        try {
-            bos = new ByteArrayOutputStream();
-            oos = new ObjectOutputStream(bos);
-            oos.writeObject(obj);
-            oos.flush();
-            bytes = bos.toByteArray();
-        } finally {
-            if (oos != null) {
-                oos.close();
-            }
-            if (bos != null) {
-                bos.close();
-            }
-        }
-        return bytes;
-    }
-
-    public static Object toObject(byte[] bytes) throws IOException, ClassNotFoundException {
-        Object obj = null;
-        ByteArrayInputStream bis = null;
-        ObjectInputStream ois = null;
-        try {
-            bis = new ByteArrayInputStream(bytes);
-            ois = new ObjectInputStream(bis);
-            obj = ois.readObject();
-        } finally {
-            if (bis != null) {
-                bis.close();
-            }
-            if (ois != null) {
-                ois.close();
-            }
-        }
-        return obj;
     }
 }
